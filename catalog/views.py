b3123c71ad31309a648443383d2751ec8947d928
mihasim <misha.dorozhkin@mail.ru>
2023-10-16 11:48:06 +0300

    Подключил CSS и  JS

--- catalog/views.py
+++ catalog/views.py
@@ -6,4 +6,11 @@ def home(request):
 
 
 def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f'Получено сообщение: {name}, ({phone}): {message}')
     return render(request, 'catalog/contacts.html')
+
+
