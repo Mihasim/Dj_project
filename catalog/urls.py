698badd5776dc0679c21f8b58b78bac61ff97932
mihasim <misha.dorozhkin@mail.ru>
2023-10-20 12:12:37 +0300

    Создал модели Product и Category. Создал миграции для новых моделей. Настроил отображение для новых моделей в панели администратора.

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-
+from django.conf import settings
+from django.conf.urls.static import static
 from catalog.views import home, contacts
 
 urlpatterns = [
     path('', home),
     path('contacts/', contacts),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
